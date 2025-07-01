0c830a2b0a03610ae9b8f521816dc7163bafa4f5
MuhammedNiyad <niyadpunniyath007@gmail.com>
2025-07-01 18:56:43 +0530

    updated

--- gsap-cc-starter/src/pages/GsapText.jsx
+++ gsap-cc-starter/src/pages/GsapText.jsx
@@ -1,5 +1,30 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to("#text", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power1.inOut",
+    })
+
+    gsap.fromTo(".para", {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+    },{
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 1,
+      stagger: 0.5,
+      ease: "power1.inOut",
+    });
+
+  }, []);
 
   return (
     <main>
